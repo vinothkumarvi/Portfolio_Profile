eb0ca18f508eb386d34c13e048be97b0cd55e4f7
vinothkumarvi <vinothkumarvi2018@gmail.com>
2024-03-05 21:28:55 +0530

    05-02-2024 process

--- assets/styles/contact.js
+++ assets/styles/contact.js
@@ -11,19 +11,19 @@ e.preventDefault();
 console.log("Clicked")
 })
 
-// function sendEmail(){
-//     Email.send({
-//         Host:"smtp.gmail.com",
-//         Username: "vinothvkm135@gmail.com",
-//         Password: "vinoth123",
-//         To: "kingvkm135@gmail.com",
-//         From: document.getElementById("email").value,
-//         Subject: document.getElementById("subject").value,
-//         Body: document.getElementById("message").value
-//     }).then(
-//         message => alert("Email sent successfully!")
-//     );
-// }
+function sendEmail(){
+    Email.send({
+        Host:"smtp.elasticemail.com",
+        Username: "vinothvkm135@gmail.com",
+        Password: "62E83080537A52282A1438D58BDCDE7C35D6503A0F8E83B84D6E69AE8CE778A6724B99F7232E21CDA9627D72CAC7BBDA",
+        To: "kingvkm135@gmail.com",
+        From: document.getElementById("email").value,
+        Subject: document.getElementById("subject").value,
+        Body: document.getElementById("message").value
+    }).then(
+        message => alert("Email sent successfully!")
+    );
+}
 
 document.getElementById('emailForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
